2a3ba5b074b44e4f46aa3b0b37397d201ae9eb57
kranz-kellar <kranz.kellar@gmail.com>
2019-12-18 13:45:54 +0300

    Removed printfs from ThreadPool

--- Erbium/ThreadPool.cpp
+++ Erbium/ThreadPool.cpp
@@ -7,6 +7,7 @@ Erbium::ThreadPool::ThreadPool()
 	for (uint32_t i = 0; i < maxThreadNums; i++) {
 		workers.push_back(thread(&ThreadPool::InfiniteWork, this));
 	}
+
 }
 
 Erbium::ThreadPool::~ThreadPool()
@@ -16,17 +17,11 @@ Erbium::ThreadPool::~ThreadPool()
 void Erbium::ThreadPool::InfiniteWork()
 {
 	while (workInProgress) {
-		printf("Waiting for job!\n");
 		unique_lock<mutex> lock(poolMutex);
 		if (!jobs.empty()) {
 			auto job = jobs.front();
 			jobs.pop();
 			job();
-			printf("Job complete!\n");
-		}
-		else {
-			std::this_thread::sleep_for(chrono::milliseconds(1));
-			printf("End of waiting!\n");
 		}
 	}
 }
--- Erbium/main.cpp
+++ Erbium/main.cpp
@@ -12,16 +12,13 @@ mutex Logger::logMutex;
 map<EventType, vector<Module*>> EventManager::subscribedModules;
 
 void sum() {
-	std::this_thread::sleep_for(chrono::milliseconds(500));
 	printf("sum1\n");
 }
 
 void sum2() {
-	std::this_thread::sleep_for(chrono::milliseconds(600));
 	printf("sum2\n");
 }
 void sum3() {
-	std::this_thread::sleep_for(chrono::milliseconds(700));
 	printf("sum3\n");
 }
 
@@ -40,7 +37,7 @@ int main()
 	threadPool.AddJob(sum2);
 	threadPool.AddJob(sum3);
 	
-	std::this_thread::sleep_for(chrono::milliseconds(1000));
+	std::this_thread::sleep_for(chrono::milliseconds(100));
 	threadPool.Terminate();
 	printf("It's works!");
 
