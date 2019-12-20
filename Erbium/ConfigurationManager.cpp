63535767413c40c044c74282bfceb6048db896c2
kranz-kellar <kranz.kellar@gmail.com>
2019-12-20 13:38:05 +0300

    Added ConfigurationManager class

---
+++ Erbium/ConfigurationManager.cpp
@@ -0,0 +1,12 @@
+#include "ConfigurationManager.h"
+
+void Erbium::ConfigurationManager::LoadConfigurations()
+{
+	pugi::xml_document document;
+	pugi::xml_parse_result result = document.load_file(CONFIG_FILE);
+	if (!result) {
+		Logger::Log(LOG_CRITICAL, "Config file not found");
+		return;
+	}
+	
+}
---
+++ Erbium/ConfigurationManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <pugixml/pugixml.hpp>
+#include "../utils/Logger.h"
+
+#define CONFIG_FILE "config.ini"
+
+namespace Erbium {
+
+	class ConfigurationManager
+	{
+	public:
+		static void LoadConfigurations();
+	};
+
+}
